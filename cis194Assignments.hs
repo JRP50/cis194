24f2c0664e222048d065c14283d9a1b1c06edcd5
James Potter <sanotter@gmail.com>
2018-04-04 13:16:24 +0100

    Final question for assignment 3

--- cis194Assignments.hs
+++ cis194Assignments.hs
@@ -104,15 +104,21 @@ testWhatWentWrong parse whatWentWrong file
   
 
   
----- Assignment 3 ------
+---- Assignment 3 - Code golf -----
 
 skips :: [a] -> [[a]]
 skips xs = [ [x | (x, y) <- zip xs (cycle [1..z]),  z == y] | z <- [1..(length xs)] ]
 
-localMaxima :: [Integer] -> [Integer]
+localMaxima :: [Int] -> [Int]
 localMaxima xs
     | length xs < 3 = []
     | otherwise     = if y > x && y > z then y:localMaxima l else localMaxima l
         where (x:l@(y:z:_)) = xs
+        
+histogram :: [Int] -> String
+histogram xs = unlines . reverse $ [['0'..'9']] ++ ["=========="] ++ hist
+    where hist = map (foldl' (\acc x -> acc ++ (take (x - (length acc)) (cycle " ")) ++ "*") []) . transpose . group $ sort xs 
+
+
 
 
