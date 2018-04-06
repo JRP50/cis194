abe9afd793e0387a0271734b1140f94f6ec22758
James Potter <sanotter@gmail.com>
2018-04-07 00:13:33 +0100

    Finished assignment 5

--- cis194Assignment5.hs
+++ cis194Assignment5.hs
@@ -1,34 +1,37 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 ---- Assignment 5 -----
 import Parser
+import StackVM
 
+-- commented out for last exercise
+--data ExprT = Lit Integer
+--           | Add ExprT ExprT
+--           | Mul ExprT ExprT
+--    deriving (Show, Eq)
 
-data ExprT = Lit Integer
-           | Add ExprT ExprT
-           | Mul ExprT ExprT
-    deriving (Show, Eq)
+-- eval :: ExprT -> Integer
+-- eval (Lit x)   = x
+-- eval (Add x y) = (eval x) + (eval y)
+-- eval (Mul x y) = (eval x) * (eval y)
 
-eval :: ExprT -> Integer
-eval (Lit x)   = x
-eval (Add x y) = (eval x) + (eval y)
-eval (Mul x y) = (eval x) * (eval y)
-
-evalStr :: String -> Maybe Integer
-evalStr s = do
-    parsedExpr <- parseExp Lit Add Mul s
-    return $ eval parsedExpr
+-- evalStr :: String -> Maybe Integer
+-- evalStr s = do
+    -- parsedExpr <- parseExp Lit Add Mul s
+    -- return $ eval parsedExpr
     
 class Expr a where
     lit :: Integer -> a
     add :: a -> a -> a
     mul :: a -> a -> a
     
-instance Expr ExprT where
-    lit = Lit
-    add = Add
-    mul = Mul
+--instance Expr ExprT where
+--    lit = Lit
+--    add = Add
+--    mul = Mul
 
-reify :: ExprT -> ExprT
-reify = id
+--reify :: ExprT -> ExprT
+--reify = id
 
 newtype MinMax = MinMax Integer deriving (Eq, Show)
 newtype Mod7 = Mod7 Integer deriving (Eq, Show)
@@ -54,5 +57,19 @@ instance Expr Mod7 where
     add (Mod7 x) (Mod7 y) = Mod7 $ x + y
     mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
     
+reify :: Program -> Program
+reify = id
 
+-- doesn't take into account bodmas
+instance Expr Program where
+    lit = (:[]) . PushI
+    add a b = a ++ b ++ [Add]
+    mul a b = a ++ b ++ [Mul]
 
+eval :: Program -> Maybe Integer
+eval p = case stackVM p of
+    (Right (IVal x)) -> return x
+    _                -> Nothing
+    
+evalStr :: String -> Maybe Integer
+evalStr s = parseExp lit add mul s >>= \parsedExpr -> eval parsedExpr
