c3eb17fddddb3eb78113f253bea1fb600e5947b2
James Potter <sanotter@gmail.com>
2018-04-07 21:01:16 +0100

    Finished assignment 6

--- cis194Assignment6.hs
+++ cis194Assignment6.hs
@@ -19,4 +19,33 @@ fibs2 = standardFib 0 1
 fancyFib :: [Integer]
 fancyFib = zipWith (+) (0:1:fancyFib) (1:fancyFib)
 
-data Stream a = Cons a (Stream a) deriving Show
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x y) = x : streamToList y
+
+instance Show a => Show (Stream a) where
+    show = show . take 20 . streamToList
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x y) =  Cons (f x) (streamMap f y)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f seed = Cons seed (streamFromSeed f newSeed)
+    where newSeed = f seed
+
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
+test' :: Stream Integer
+test' = test 2
+
+test :: Int -> Stream Integer
+test n = Cons 1 (Cons n )
+
+-- come back to ruler, need to interleave 0's with multiples of 2 in the form
+-- 1 2 1 3 1 2 1 4 1 2 1 3 1 2 1 5, etc. 
+-- so the algo is something like take list, add n+1 to the end, append original list to the end, and add n+2 and repeat
