b33f9db7e6d98c77973df98378bd1a710e4852c4
James Potter <sanotter@gmail.com>
2018-04-15 14:57:06 +0100

    Finished ex.1,2 of assignment 12

--- 12-monads/cis194Assignment12.hs
+++ 12-monads/cis194Assignment12.hs
@@ -1,3 +1,22 @@
 -- Assignment 12 --
 import Risk
+import Control.Monad.Random
+import Data.List
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle b@(Battlefield att def) = liftM2 (newBattleField b) aRolls dRolls
+    where 
+        aRolls = nSortedDieRolls $ min 3 (att - 1)
+        dRolls = nSortedDieRolls $ min 2 def
+
+nSortedDieRolls :: Int -> Rand StdGen [DieValue]
+nSortedDieRolls n = liftM (reverse . sort) . sequence $ replicate n die
+
+newBattleField :: Battlefield -> [DieValue] -> [DieValue] -> Battlefield
+newBattleField (Battlefield oldA oldD) att def 
+    = Battlefield (oldA - attLost) (oldD - defLost)
+        where 
+            defLost = length $ filter (==True) result
+            attLost = length $ filter (==False) result
+            result  = zipWith (>) att def
 
